215aa5648b0ab63791103ec689e880eb55594a7f
JordanCCarr <jcarr1624@gmail.com>
2023-01-12 22:42:25 -0500

    update url in about me

--- src/components/Body/AboutMe/AboutMe.tsx
+++ src/components/Body/AboutMe/AboutMe.tsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
         <div className="about-me-wrapper">
             <div className="about-text">
                 <p>I'm a software developer, minimalist, and lifelong fitness enthusiast.</p>
-                <p>My technical capabilties include both backend and frontend development. <a href="https://github.com/JordanCCarr/Personal-Website">I even wrote this website myself!</a></p>
+                <p>My technical capabilties include both backend and frontend development. <a href="https://github.com/JordanCCarr/JordanCCarr.github.io">I even wrote this website myself!</a></p>
                 <p>Since graduating from the University of Pittsburgh in 2018 I have been a full time software developer. When not coding
                     you can find me outside enjoying nature, preparing for my next marathon, or enjoying the sights and sounds that travel has to offer.
                 </p>
